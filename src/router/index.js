de610d119bde7ca41d2ca1f86cd1a057695dbc99
spavlishak <sophiej1652@gmail.com>
2017-11-02 17:20:44 +0200

    Added basic pages

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,27 @@ export default new Router({
       component: {
         template: require('@/views/home.html')
       }
+    },
+    {
+      path: '/bootcamp',
+      name: 'Bootcamp',
+      component: {
+        template: require('@/views/bootcamp.html')
+      }
+    },
+    {
+      path: '/labs-and-events',
+      name: 'Labs and Events',
+      component: {
+        template: require('@/views/labs-and-events.html')
+      }
+    },
+    {
+      path: '/contact-us',
+      name: 'Contact us',
+      component: {
+        template: require('@/views/contact-us.html')
+      }
     }
   ]
 })
